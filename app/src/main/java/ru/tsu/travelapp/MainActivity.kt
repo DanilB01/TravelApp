07ef07a5a92b5c6e7d4054dd853006082c39299b
Danil Berdnikov <danil.b0101@bk.ru>
2022-09-09 00:23:09 +0700

    Add search and descriptions states

--- app/src/main/java/ru/tsu/travelapp/MainActivity.kt
+++ app/src/main/java/ru/tsu/travelapp/MainActivity.kt
@@ -1,10 +1,7 @@
 package ru.tsu.travelapp
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
@@ -24,9 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -35,7 +30,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -64,12 +58,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
         setContent {
-            SetupView()
+            TravelAppTheme {
+                SetupView()
+            }
         }
     }
 }
@@ -77,7 +69,9 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 private fun Preview() {
-    SetupView()
+    TravelAppTheme {
+        SetupView()
+    }
 }
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -171,6 +165,16 @@ private fun NavGraph(navController: NavHostController) {
 @Composable
 private fun MainScreen() {
     var searchQuery by remember { mutableStateOf("") }
+    var isSearchState by remember { mutableStateOf(false)}
+
+    var cityDetails by remember { mutableStateOf(Pair("","")) }
+    if(cityDetails.first.isNotBlank()) {
+        DetailsScreen(cityDetails) {
+            cityDetails = Pair("","")
+        }
+        return
+    }
+
     var showDialog by remember { mutableStateOf(false) }
     if (showDialog) {
         AlertDialog(
@@ -193,7 +197,21 @@ private fun MainScreen() {
 
     val imageUrl =
         "https://mir-s3-cdn-cf.behance.net/project_modules/fs/731afa34542037.56d4c4da102e7.jpg"
-    val images = List(5) { imageUrl }
+
+    val cities = listOf(
+        Pair("Новокузнецк", imageUrl),
+        Pair("Екатеринбург", imageUrl),
+        Pair("Калининград", imageUrl),
+        Pair("Владивосток", imageUrl),
+        Pair("Петропавловск-Камчатский", imageUrl),
+        Pair("Иркутск", imageUrl),
+        Pair("Казань", imageUrl),
+        Pair("Воркута", imageUrl),
+        Pair("Магадан", imageUrl),
+        Pair("Архангельск", imageUrl)
+    )
+
+    var currentCities by remember { mutableStateOf(cities) }
 
     TravelAppTheme {
         LazyColumn(
@@ -204,7 +222,13 @@ private fun MainScreen() {
                 Spacer(Modifier.size(30.dp))
                 TextField(
                     value = searchQuery,
-                    onValueChange = { searchQuery = it },
+                    onValueChange = {
+                        searchQuery = it
+                        isSearchState = searchQuery.isNotEmpty()
+                        currentCities = cities.filter {
+                            it.first.contains(searchQuery, ignoreCase = true)
+                        }
+                    },
                     leadingIcon = {
                         Icon(
                             imageVector = ImageVector.vectorResource(id = R.drawable.ic_search),
@@ -231,67 +255,74 @@ private fun MainScreen() {
                         .padding(horizontal = 32.dp)
                         .shadow(1.dp)
                 )
-                Text(
-                    text = "Рекомендации",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(top = 25.dp, start = 22.dp)
-                )
-                Spacer(modifier = Modifier.size(8.dp))
-                LazyRow(
-                    contentPadding = PaddingValues(horizontal = 22.dp),
-                    horizontalArrangement = Arrangement.spacedBy(15.dp),
-                ) {
-                    items(images) { image ->
-                        Box(
-                            modifier = Modifier.clickable(
-                                onClick = {
-                                    showDialog = true
-                                }
-                            )
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.pic_place),
-                                contentDescription = null,
-                                contentScale = ContentScale.FillBounds,
-                                modifier = Modifier
-                                    .size(width = 125.dp, height = 208.dp)
-                                    .clip(RoundedCornerShape(8.dp))
-                                    .align(Alignment.BottomCenter)
-                            )
-                            AsyncImage(
-                                model = image,
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(width = 145.dp, height = 200.dp)
-                                    .clip(RoundedCornerShape(8.dp))
-                            )
-                            Text(
-                                text = "Ульяновск",
-                                fontSize = 14.sp,
-                                color = colorResource(id = R.color.white),
-                                modifier = Modifier
-                                    .padding(start = 9.dp, bottom = 21.dp)
-                                    .align(Alignment.BottomStart)
-                            )
+                if(!isSearchState) {
+                    Text(
+                        text = "Рекомендации",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(top = 25.dp, start = 22.dp)
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 22.dp),
+                        horizontalArrangement = Arrangement.spacedBy(15.dp),
+                    ) {
+                        items(cities) { city ->
+                            Box(
+                                modifier = Modifier.clickable(
+                                    onClick = {
+                                        showDialog = true
+                                    }
+                                )
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.pic_place),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.FillBounds,
+                                    modifier = Modifier
+                                        .size(width = 125.dp, height = 208.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                        .align(Alignment.BottomCenter)
+                                )
+                                AsyncImage(
+                                    model = city.second,
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier
+                                        .size(width = 145.dp, height = 200.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                )
+                                Text(
+                                    text = city.first,
+                                    fontSize = 14.sp,
+                                    color = colorResource(id = R.color.white),
+                                    modifier = Modifier
+                                        .padding(start = 9.dp, bottom = 21.dp)
+                                        .align(Alignment.BottomStart)
+                                )
+                            }
                         }
                     }
+                    Text(
+                        text = "Популярное",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(top = 30.dp, start = 22.dp)
+                    )
+                    Spacer(modifier = Modifier.size(12.dp))
+                } else {
+                    Spacer(modifier = Modifier.size(35.dp))
                 }
-                Text(
-                    text = "Популярное",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(top = 30.dp, start = 22.dp)
-                )
-                Spacer(modifier = Modifier.size(12.dp))
             }
-            items(images) { image ->
+            items(currentCities) { city ->
                 Box(
                     modifier = Modifier.padding(horizontal = 22.dp)
+                        .clickable {
+                            cityDetails = city
+                        }
                 ) {
                     AsyncImage(
-                        model = image,
+                        model = city.second,
                         contentDescription = null,
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
@@ -300,7 +331,7 @@ private fun MainScreen() {
                             .clip(RoundedCornerShape(8.dp))
                     )
                     Text(
-                        text = "Ульяновск",
+                        text = city.first,
                         fontSize = 14.sp,
                         color = colorResource(id = R.color.white),
                         modifier = Modifier
@@ -313,6 +344,56 @@ private fun MainScreen() {
             item {
                 Spacer(modifier = Modifier.size(51.dp))
             }
+        }
+    }
+}
+
+@Composable
+private fun DetailsScreen(cityDetails: Pair<String, String>, exit: () -> Unit) {
+    LazyColumn {
+        item {
+            Box {
+                AsyncImage(
+                    model = cityDetails.second,
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(225.dp)
+                        .clip(RoundedCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp))
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(26.dp)
+                        .size(height = 15.dp, width = 30.dp)
+                        .clickable { exit() }
+                )
+                Text(
+                    text = cityDetails.first,
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(start = 26.dp, bottom = 16.dp)
+                )
+            }
+            Spacer(modifier = Modifier.size(30.dp))
+            Text(
+                text = "Кра́сная пло́щадь — главная площадь Москвы, расположена между Московским Кремлём (к западу) и Китай-городом (на восток). Выходит к берегу Москвы-реки через пологий Васильевский спуск. Площадь тянется вдоль северо-восточной стены Кремля, от Кремлёвского проезда и проезда Воскресенские Ворота до Васильевского спуска, выходящего к Кремлёвской набережной. На восток от Красной площади отходят Никольская улица, Ильинка и Варварка. Вдоль западной стороны площади расположен Московский Кремль, вдоль восточной — Верхние торговые ряды и Средние торговые ряды. Входит в единый ансамбль с Московским Кремлём, однако исторически является частью Китай-города",
+                color = colorResource(id = R.color.description_color),
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .padding(horizontal = 30.dp)
+            )
+            Spacer(modifier = Modifier.size(30.dp))
+            Text(
+                text = "Рейтинг",
+                color = colorResource(id = R.color.title_color),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(horizontal = 30.dp)
+            )
         }
     }
 }
